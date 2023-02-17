a97cf92296253f9fc0f04aed358a49c10d0eae5d
Julian Celeita <andresceleita@yahoo.es>
2023-02-17 15:17:50 -0500

    Inputs and button addeds

--- app/components/login/Header.tsx
+++ app/components/login/Header.tsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <div className="sticky w-full top-0 left-0 bg-inherit flex items-center justify-between p-4 px-6 border-b border-solid border-white">
+    <div className="sticky w-full top-0 left-0 bg-inherit flex items-center justify-between p-4 border-b border-solid border-white">
         <h1 className="text-3xl sm:text-6xl select-none"> LOGO </h1>
         <i className="fa-solid fa-user text-xl sm:text-3xl duration-300 hover:opacity-40 cursor-pointer"></i>
     </div>
--- app/components/login/Main.tsx
+++ app/components/login/Main.tsx
@@ -1,7 +1,26 @@
 function Main() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="flex-1 text-white">LoginInpusdat</div>
+    <div className="mt-28 flex-1 text-xs sm:text-sm flex flex-col justify-center items-center gap-2 sm:gap-4">
+      <h1 className="font-extrabold select-none text-2xl sm:text-4xl">LOGIN</h1>
+      <input
+        type="text"
+        placeholder="Username / Email"
+        className="outline-none duration-300 border-b-2 border-solid border-white focus:border-orange-600
+         text-slate-900 px-2 py-1 w-full max-w-[40ch]"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        className="outline-none duration-300 border-b-2 border-solid border-white focus:border-orange-600
+         text-slate-900 px-2 py-1 w-full max-w-[40ch]"
+      />
+      <button
+        className="w-full max-w-[40ch] border border-white border-solid uppercase py-2 duration-300 relative
+         after:absolute after:top-0 after:right-full after:bg-white after:z-10 after:w-full after:h-full 
+         overflow-hidden hover:after:translate-x-full after:duration-300 hover:text-slate-900 font-bold hover:border-orange-600"
+      >
+        <h2 className="relative z-20">Submit</h2>
+      </button>
     </div>
   );
 }
